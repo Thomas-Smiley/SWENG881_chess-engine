0e364dfde6b156c9fa49def84e8609c320e498fb
Thomas-Smiley <tvs5939@psu.edu>
2024-07-14 16:59:41 -0600

    Setup testing environment.

---
+++ tests/test_ChessEngine.py
@@ -0,0 +1,12 @@
+import unittest
+
+from chess import ChessEngine
+
+
+class TestGameState(unittest.TestCase):
+    def test_makeMove(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
