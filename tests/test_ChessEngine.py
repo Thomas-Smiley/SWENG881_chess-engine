68347407739ea22edb98e8560658836544825713
Brian <Brian.A.Noss@gmail.com>
2024-08-04 18:02:02 -0400

    adding base test

--- tests/test_ChessEngine.py
+++ tests/test_ChessEngine.py
@@ -1,11 +1,21 @@
 import unittest
+import chess
+import chess.ChessAI
+import chess.ChessEngine
+import chess.ChessMain
 
 from chess import ChessEngine
 
 
 class TestGameState(unittest.TestCase):
     def test_makeMove(self):
-        pass
+        self.assertEqual(True, True)
+
+    def test_inCheck(self):
+        self.assertEqual(True, True)
+
+
+
 
 
 if __name__ == '__main__':
